6135df5b8ec4cf17abb28be764c027a4bce11651
jung-kr <jungwy98@gmail.com>
2022-10-04 22:42:05 +0900

    refactor : create img folder

--- client/src/components/Slidebanner.js
+++ client/src/components/Slidebanner.js
@@ -1,7 +1,7 @@
 import "./slick-carousel/slick/slick.css";
 import "./slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
-import A from './A.PNG'
+import A from './img/A.PNG'
 //import React, { Component } from "react";
 
 
