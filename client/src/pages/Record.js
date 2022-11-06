2548f3b80dbd1d3227b1279ac218973b6f6585c0
Lee-Gyeore <lkr9902@naver.com>
2022-11-07 01:19:13 +0900

    feat : Record page calendar

--- client/src/pages/Record.js
+++ client/src/pages/Record.js
@@ -1,13 +1,53 @@
-import React from 'react';
+// import React from 'react';
+// import '../App.css'
+// import Sidebar from '../components/Sidebar.js'
+
+
+// function Record() {
+//     return (
+//         <>
+//             <Sidebar/>
+//             <h1 className="myFarmRecord">MyFarm-Record</h1>
+        
+//         </>
+//     );
+// }
+
+// export default Record;
+
+
+import React, { useState } from 'react';
 import '../App.css'
 import Sidebar from '../components/Sidebar.js'
+import Calendar from 'react-calendar';
+import './Record.css';
+import moment from "moment";
 
 function Record() {
+    const [value, onChange] = useState(new Date());
+
+    const marks = [     //날짜 마킹 일 월 년 순
+        "08-11-2022"
+      ];
+
+    
+
     return (
         <>
             <Sidebar/>
-            <h1 className="myFarmRecord">MyFarm-Record</h1>
-        
+            <Calendar
+                onChange={onChange}
+                value={value}
+                locale="en-EN"
+                tileClassName={({ date, view }) => {
+                    if (marks.find((x) => x === moment(date).format("DD-MM-YYYY"))) {
+                        return "highlight";
+                    }
+                }}
+            />
+
+            
+
         </>
     );
 }
