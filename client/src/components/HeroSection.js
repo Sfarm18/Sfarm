fa04ce0f8e213df688637228dc36a64e924af15a
Lee-Gyeore <lkr9902@naver.com>
2022-11-29 21:48:42 +0900

    fix: Home page img

--- client/src/components/HeroSection.js
+++ client/src/components/HeroSection.js
@@ -2,12 +2,19 @@ import React from 'react'
 import { Button } from './Button';
 import './HeroSection.css'
 import T1 from '../asset/images/tree1.png'
+import T2 from '../asset/images/tree6.png'
+import T3 from '../asset/images/tree7.png'
+import T4 from '../asset/images/tree9.png'
 
 function HeroSection() {
   return (
     <section className='hero-container'>
         <h1 className='title'>Smart farm</h1>
         <img className="img1" src={T1} alt="T1"/>
+        <img className="img2" src={T2} alt="T2"/>
+        <img className="img3" src={T3} alt="T3"/>
+        <img className="img4" src={T4} alt="T4"/>
+
         <div className="hero-btns">
             <Button 
                 className='addfarm-btns' 
