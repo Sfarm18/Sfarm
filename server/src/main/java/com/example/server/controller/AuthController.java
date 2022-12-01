b7f203cd234ce5b71d0664930270175c090cda99
jung-kr <jungwy98@gmail.com>
2022-12-02 03:37:47 +0900

    feat : AuthController

---
+++ server/src/main/java/com/example/server/controller/AuthController.java
@@ -0,0 +1,29 @@
+package com.example.server.controller;
+
+import com.example.server.dto.MemberRequestDto;
+import com.example.server.dto.MemberResponseDto;
+import com.example.server.dto.TokenDto;
+import com.example.server.service.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+    private final AuthService authService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<MemberResponseDto> signup(@RequestBody MemberRequestDto requestDto) {
+        return ResponseEntity.ok(authService.signup(requestDto));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<TokenDto> login(@RequestBody MemberRequestDto requestDto) {
+        return ResponseEntity.ok(authService.login(requestDto));
+    }
+}
